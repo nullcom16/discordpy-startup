5fb527a9e8da4ce5db0e65033e4dca57a25e2082
nullcom16 <82245791+nullcom16@users.noreply.github.com>
2021-04-11 16:59:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,51 @@
+import discord
+import asyncio
 from discord.ext import commands
-import os
-import traceback
+import subprocess
+import ffmpeg
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = commands.Bot(command_prefix='.')
+voice_client = None
+@client.event
+async def on_ready():
+    print(client.user.name + ' 起動')
 
+@client.command()
+async def join(ctx):
+    vc = ctx.author.voice.channel
+    await vc.connect()
+    mes = "ワイはアル太郎や読み上げたるでぇ"
+    await ctx.send(mes)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.command()
+async def bye(ctx):
+    await ctx.voice_client.disconnect()
+    await ctx.send("ほなさいなら")
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('.'):
+        pass
+    elif message.author.bot:
+        pass
+    else:
+        mes = message.content
+        create_wav(mes)
+        ffmpeg_audio_source = discord.FFmpegPCMAudio("open_jtalk.wav")
+        message.guild.voice_client.play(ffmpeg_audio_source)
+    await client.process_commands(message)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+def create_wav(txt):
+    open_jtalk=['open_jtalk']
+    mech=['-x','/usr/local/share/open_jtalk/open_jtalk_dic_utf_8-1.10']
+    htsvoice=['-m','/usr/local/share/hts_voice/mei/mei_normal.htsvoice']
+    speed=['-r','1.0']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+    c.stdin.write(txt.encode('utf-8'))
+    c.stdin.close()
+    c.wait()
 
-
-bot.run(token)
+client.run(token)
